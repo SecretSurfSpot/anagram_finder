f28da11b8759a8b4e7f9edf3132b6927e6410b39
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-12-12 15:25:04 +0000

    test: added Printer.print_array unit test

---
+++ spec/unit/printer_spec.rb
@@ -0,0 +1,12 @@
+require 'printer'
+
+describe Printer do
+  printer = Printer.new
+  anagram_array = ['silent','enlist','tinsel']
+
+  describe '#print_array' do
+    it 'should print all words in a given array' do
+      expect { printer.print_anagrams(anagram_array) }.to output(/"silent"\n"enlist"\n"tinsel"/).to_stdout
+    end
+  end
+end
