55336c5eac70e5cd234281a970bdb5b8f8e1fd13
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-12-12 15:22:54 +0000

    feat: added AnagramFinder.create_anagrams_array class & method

---
+++ lib/anagram_finder.rb
@@ -0,0 +1,15 @@
+# AnagramFinder this class take a word (subject) and an array as inputs
+# and creates an anagrams_array of words from the array that
+# are anagrams of the subject
+class AnagramFinder
+  def create_anagrams_array(subject, word_array)
+    anagrams_array = []
+    word_array.each do |word|
+      if word.downcase.chars.sort.join == subject.downcase.chars.sort.join
+        anagrams_array << word
+      end
+        anagrams_array.include? subject.downcase ? anagrams_array.delete(subject.downcase) : false
+    end
+    return anagrams_array
+  end
+end
