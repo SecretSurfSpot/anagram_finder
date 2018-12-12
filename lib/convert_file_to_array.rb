392e0a276182f2f55c811f8776db4735841c4e36
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-12-12 15:11:03 +0000

    refactor: modifications in line with Rubocop

--- lib/convert_file_to_array.rb
+++ lib/convert_file_to_array.rb
@@ -1,8 +1,10 @@
+# ConvertFileToArray - this class contains methods that:
+# converts the contents of a txt file into an array
 class ConvertFileToArray
   def convert_txt_file_to_array(file)
     word_array = []
     file.readlines.each do |word|
-        word_array << word.strip
+      word_array << word.strip
     end
     return word_array
   end
